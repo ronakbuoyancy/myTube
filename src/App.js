3bf75144fbdc75da44d7df9c2c9866756cc30090
Girish Vadhel <girishvadhel88@gmail.com>
2022-11-03 18:48:27 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import VideoScreen from './components/VideoScreen.js/VideoScreen';
 import { Routes, Route } from 'react-router-dom'
 
 function App() {
-  //Route
   return (
     <div className="App">
       <Routes>
